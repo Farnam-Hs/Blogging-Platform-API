993edd86b27c45926283e9dcac70aa52d2d0a96a
Farnam <farnam.hs@yahoo.com>
2024-09-23 13:05:28 +0330

    Implemented Service

--- src/main/java/com/farnamhs/blogging/dto/PostRequestDto.java
+++ src/main/java/com/farnamhs/blogging/dto/PostRequestDto.java
@@ -1,15 +1,5 @@
 package com.farnamhs.blogging.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import java.util.List;
 
-public record PostRequestDto(@JsonProperty("title")
-                             String title,
-                             @JsonProperty("content")
-                             String content,
-                             @JsonProperty("category")
-                             String category,
-                             @JsonProperty("tags")
-                             List<String> tags) {
-}
+public record PostRequestDto(String title, String content, String category, List<String> tags) {}
--- src/main/java/com/farnamhs/blogging/dto/PostResponseDto.java
+++ src/main/java/com/farnamhs/blogging/dto/PostResponseDto.java
@@ -1,24 +1,6 @@
 package com.farnamhs.blogging.dto;
 
-import com.farnamhs.blogging.config.InstantSerializer;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-
 import java.time.Instant;
 import java.util.List;
 
-public record PostResponseDto(@JsonProperty("id")
-                              long id,
-                              @JsonProperty("title")
-                              String title,
-                              @JsonProperty("content")
-                              String content,
-                              @JsonProperty("category")
-                              String category,
-                              @JsonProperty("tags")
-                              List<String> tags,
-                              @JsonProperty("createdAt") @JsonSerialize(using = InstantSerializer.class)
-                              Instant createdAt,
-                              @JsonProperty("updatedAt") @JsonSerialize(using = InstantSerializer.class)
-                              Instant updatedAt) {
-}
+public record PostResponseDto(long id, String title, String content, String category, List<String> tags, Instant createdAt, Instant updatedAt) {}
